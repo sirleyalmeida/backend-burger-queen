38b1d32bcc2157eb6797c773f5ce85c944fd3852
sirleyalmeida <sirleyas90@gmail.com>
2019-07-15 17:31:11 -0300

    start add no package.json

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const db = require('./models/index');
-const port = 3269;
+const port = process.env.port || '8080';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, (req, resp) => {
-    console.log('Funciona')
+    console.log('Burger Queen API')
 });
 
 app.use('/users', require('./routes/users'));
