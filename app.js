4016fb4496c021967cc103c85f340b8ff825bb9d
sirleyalmeida <sirleyas90@gmail.com>
2019-07-17 15:11:30 -0300

    add app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require('./models/index');
-const port = process.env.port || '8080';
+const port = process.env.PORT || '8080';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
